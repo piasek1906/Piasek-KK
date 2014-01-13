8172152c3132caabe35e3dcbe6e27c5edeaf4529
piasek1906 <piasek1906@gmail.com>
2014-01-13 02:02:57 +0100

    ARM optimalization  ARM: Use -mcpu=cortex-a15 when targeting MSM Krait CPUs ARM: mm: implement LoUIS API for cache maintenance ops  ARM: kernel: update cpu_suspend code to use cache LoUIS operations ARM: kernel: update __cpu_disable to use cache LoUIS maintenance API ARM: mm: update __v7_setup() to the new LoUIS cache maintenance API ARM: 7408/1: cacheflush: return error to userspace when flushing syscall fails  ARM: cacheflush: split user cache-flushing into interruptible chunks … ARM: cacheflush: don't bother rounding to nearest vma ARM: 7917/1: cacheflush: correctly limit range of memory region being flushed ARM: mm: rename jump labels in v7_flush_dcache_all function ARM: 7461/1: topology: Add arch_scale_freq_power function ARM: 7462/1: topology: factorize the update of sibling masks ARM: 7463/1: topology: Update cpu_power according to DT information ARM: 7482/1: topology: fix section mismatch warning for init_cpu_topology

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -502,28 +502,64 @@ static int bad_syscall(int n, struct pt_regs *regs)
 	return regs->ARM_r0;
 }
 
-static inline void
+static long do_cache_op_restart(struct restart_block *);
+
+static inline int
+__do_cache_op(unsigned long start, unsigned long end)
+{
+	int ret;
+
+	do {
+		unsigned long chunk = min(PAGE_SIZE, end - start);
+
+		if (signal_pending(current)) {
+		   struct thread_info *ti = current_thread_info();
+
+		   ti->restart_block = (struct restart_block) {
+			.fn  = do_cache_op_restart,
+			};
+
+		   ti->arm_restart_block = (struct arm_restart_block) {
+			{
+			   .cache = {
+				.start  = start,
+				.end  = end,
+			},
+		},
+	};
+
+	return -ERESTART_RESTARTBLOCK;
+   }
+
+	ret = flush_cache_user_range(start, start + chunk);
+	if (ret)
+		return ret;
+
+	cond_resched();
+	start += chunk;
+	} while (start < end);
+
+	return 0;
+}
+
+static long do_cache_op_restart(struct restart_block *unused)
+{
+	struct arm_restart_block *restart_block;
+
+	restart_block = &current_thread_info()->arm_restart_block;
+	return __do_cache_op(restart_block->cache.start,
+			restart_block->cache.end);
+}
+
+static inline int
 do_cache_op(unsigned long start, unsigned long end, int flags)
 {
-	struct mm_struct *mm = current->active_mm;
-	struct vm_area_struct *vma;
-
 	if (end < start || flags)
-		return;
-
-	down_read(&mm->mmap_sem);
-	vma = find_vma(mm, start);
-	if (vma && vma->vm_start < end) {
-		if (start < vma->vm_start)
-			start = vma->vm_start;
-		if (end > vma->vm_end)
-			end = vma->vm_end;
-
-		up_read(&mm->mmap_sem);
-		flush_cache_user_range(start, end);
-		return;
-	}
-	up_read(&mm->mmap_sem);
+		return -EINVAL;
+	if (!access_ok(VERIFY_READ, start, end - start))
+		return -EFAULT;
+	
+	return __do_cache_op(start, end);
 }
 
 /*
@@ -569,8 +605,7 @@ asmlinkage int arm_syscall(int no, struct pt_regs *regs)
 	 * the specified region).
 	 */
 	case NR(cacheflush):
-		do_cache_op(regs->ARM_r0, regs->ARM_r1, regs->ARM_r2);
-		return 0;
+		return do_cache_op(regs->ARM_r0, regs->ARM_r1, regs->ARM_r2);
 
 	case NR(usr26):
 		if (!(elf_hwcap & HWCAP_26BIT))
